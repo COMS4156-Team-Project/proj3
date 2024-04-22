18e00846bfb3ef0bfcb8f3eaa9a9261e2e5dfe38
Ajit Sharma Kasturi <ak5055@columbia.edu>
2024-04-22 02:14:31 -0400

    Dataset changes new

--- Dataset.py
+++ Dataset.py
@@ -4,10 +4,6 @@ import pandas as pd
 class Dataset(object):
     def __init__(self, path: str):
         self.df = pd.read_csv(path)
-        self._perform_data_quality_checks()
-
-    def _perform_data_quality_checks(self):
-        pass
 
     def get_items(self) -> list:
         items  = []
